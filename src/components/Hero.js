e5afb5ffca730a1b26d23d5f1c613796af55d1eb
sadaatgithub <sadachaudhari@gmail.com>
2022-11-19 18:30:51 +0530

    updated

---
+++ src/components/Hero.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ResumeButton from './buttons/ResumeButton'
+
+
+const Hero = () => {
+  return (
+    <>
+    <main
+          name="about"
+          className="hero-banner relative flex justify-center items-center w-full  text-gray-800 h-[100vh] bg-hero-pattern2 bg-cover bg-center overflow-hidden]"
+        >
+          <div
+            className="flex p-6 flex-col justify-center items-center gap-4 text-center z-10
+              flex-grow-1  border-1"
+          >
+            <div className="mt-14 text-3xl sm:text-6xl font-nunito font-semibold text-white dark:text-gray-200  sm:mt-6 overflow-hidden">
+              <h1 className="uppercase animate-slideInBottom transition-all anim-delay-500">
+                <span className="">Hi <span className="text-teal-400">,</span> I am <span className="text-teal-400">,</span> </span>
+                <span className="">Sadanand</span>
+              </h1>
+            </div>
+            <div className="overflow-hidden">
+            <h3 className="text-blue-400 uppercase text-xl mt-4 sm:text-3xl font-port-ligat-sans font-bold tracking-widest animate-slideInBottom transition-all anim-delay-800">
+              Frontend Web Developer
+            </h3>
+            </div>
+            <div className="overflow-hidden flex justify-center">
+            <p className="text-sm leading-loose w-auto text-center sm:w-1/2 sm:text-base mt-2 text-white/80 animate-slideInBottom transition-all anim-delay-900 ">
+            A Frontend focused Web Developer building the Frontend of Websites and 
+            Web Applications that leads to the success of the overall product
+            </p>
+            </div>
+           
+             <ResumeButton text={"GET RESUME"}/>
+          </div>
+        
+        </main>
+
+    </>
+  )
+}
+
+export default Hero
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,10 @@ import Projects from "./Projects";
 import About from "./About";
 import ScrollHere from "./ScrollHere";
 import Footer from "./Footer";
-import resume from "../assets/resume/srcfrontend.pdf"
+import Hero from "./Hero";
+import ResumeButton from "./buttons/ResumeButton";
+// import resume from "../assets/resume/srcfrontend.pdf"
+
 
 const Home = ({toggleTheme,toggle}) => {
   const [burgerOpen, setBurgerOpen] = useState(false);
@@ -105,12 +108,7 @@ const Home = ({toggleTheme,toggle}) => {
             CONTACT
           </Link>
         </ul>
-        <a href={resume} download
-              className="hidden sm:block px-3 py-2 bg-teal-400
-             text-white  tracking-wide rounded
-             hover:bg-teal-500 animate-fadeIn transition-all anim-delay-500
-              " value="RESUME" type="button"
-            >RESUME</a>
+        <ResumeButton text={"RESUME"}/>
 
           <button className= {`${animateNav? "text-black dark:text-teal-400":"dark:text-teal-400 text-white"} px-2 py-2 sm:ml-4 ml-0 mr-6 sm:mr-0 text-xl rounded-full`} onClick={toggleTheme} >{toggle?<MdOutlineLightMode/>: <MdDarkMode/>}</button>
        
@@ -139,44 +137,9 @@ const Home = ({toggleTheme,toggle}) => {
         <div className=" mx-auto flex flex-col h-full">
 
 {/* Hero section-------------------> */}
+<Hero/>
 
-        <main
-          name="about"
-          className="hero-banner relative flex justify-center items-center w-full  text-gray-800 h-[100vh] bg-hero-pattern2 bg-cover bg-center overflow-hidden]"
-        >
-          <div
-            className="flex p-6 flex-col justify-center items-center gap-4 text-center z-10
-              flex-grow-1  border-1"
-          >
-            <div className="mt-14 text-3xl sm:text-6xl font-nunito font-semibold text-white dark:text-gray-200  sm:mt-6 overflow-hidden">
-              <h1 className="uppercase animate-slideInBottom transition-all anim-delay-500">
-                <span className="">Hi <span className="text-teal-400">,</span> I'am <span className="text-teal-400">,</span> </span>
-                <span className="">Sadanand</span>
-              </h1>
-            </div>
-            <div className="overflow-hidden">
-            <h3 className="text-blue-400 uppercase text-xl mt-4 sm:text-3xl font-port-ligat-sans font-bold tracking-widest animate-slideInBottom transition-all anim-delay-800">
-              Frontend Web Developer
-            </h3>
-            </div>
-            <div className="overflow-hidden flex justify-center">
-            <p className="text-sm leading-loose w-auto text-center sm:w-1/2 sm:text-base mt-2 text-white/80 animate-slideInBottom transition-all anim-delay-900 ">
-            A Frontend focused Web Developer building the Frontend of Websites and 
-            Web Applications that leads to the success of the overall product
-            </p>
-            </div>
-            <a href={resume} download
-              className="py-3 flex justify-center items-center gap-4 px-8 bg-teal-500
-              mt-4 text-base text-white rounded tracking-wider font-semibold 
-               hover:shadow-sm hover:bg-teal-600 animate-slideInBottom transition-all anim-delay-1000 cursor-pointer 
-              ">GET RESUME
-             </a>
-          </div>
-          {/* <div className="flex justify-center items-center  animate-slideInLeft transition-all anim-delay-1000 p-2">
-            <img src={webdev} alt="" /
-          </div> */}
-        </main>
-
+        
         <ScrollHere/>
         <About />
         <Projects />
