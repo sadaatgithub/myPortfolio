53c7a11529ac89b334a521d3f08ecc500ead440d
sadaatgithub <sadachaudhari@gmail.com>
2022-11-25 22:25:16 +0530

    updated

--- src/components/buttons/ResumeButton.js
+++ src/components/buttons/ResumeButton.js
@@ -4,8 +4,8 @@ import resume from "../../assets/resume/srcfrontend.pdf"
 const ResumeButton = ({text}) => {
   return (
     <a href={resume} download
-              className={`${text==="RESUME"? "hidden md:block":"mt-8"} px-3 py-2 bg-teal-400
-              text-white  tracking-wide rounded hover:bg-teal-500 animate-fadeIn transition-all anim-delay-500
+              className={`${text==="RESUME"? "hidden md:block animate-fadeIn anim-delay-500":"mt-8 animate-slideInBottom anim-delay-1000"} px-3 py-2 bg-teal-400
+              text-white  tracking-wide rounded hover:bg-teal-500  transition-all 
                `} value="RESUME" type="button"
             >{text}</a>
   )
